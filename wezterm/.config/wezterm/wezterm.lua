3b1e94075281a314db620b216df78fd20d2b8dda
0xYYY <0xYYY@pm.me>
2024-02-21 23:22:26 +0800

    wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-local solarized_dark = wezterm.color.get_builtin_schemes()["Solarized Dark (base16)"]
+local solarized_dark = wezterm.color.get_builtin_schemes()["Builtin Solarized Dark"]
 solarized_dark.tab_bar = {
 	background = "#002b36",
 	active_tab = {
@@ -8,25 +8,21 @@ solarized_dark.tab_bar = {
 		fg_color = "#fdf6e3",
 		intensity = "Bold",
 	},
-
 	inactive_tab = {
 		bg_color = "#002b36",
 		fg_color = "#586e75",
 		italic = true,
 	},
-
 	inactive_tab_hover = {
 		bg_color = "#002b36",
 		fg_color = "#fdf6e3",
 		italic = false,
 		intensity = "Bold",
 	},
-
 	new_tab = {
 		bg_color = "#002b36",
 		fg_color = "#586e75",
 	},
-
 	new_tab_hover = {
 		bg_color = "#002b36",
 		fg_color = "#fdf6e3",
@@ -44,6 +40,7 @@ return {
 	color_scheme = "Solarized Dark",
 	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
 	font_size = 14,
+	bold_brightens_ansi_colors = "No",
 	initial_rows = 1000,
 	initial_cols = 1000,
 	window_decorations = "RESIZE",
